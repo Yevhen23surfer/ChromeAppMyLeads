a7c4dded85c56772eb8b078fabc5cb0b59041486
Yevhen Surfer <23skopets@gmail.com>
2024-05-27 20:39:53 +0200

    Get the leads from the localStorage

--- index.js
+++ index.js
@@ -3,20 +3,24 @@ const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
+// localStorage.clear()
+let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+console.log(leadsFromLocalStorage)
+
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   inputEl.value = "";
   // Save the myLeads array to localStorage 
   localStorage.setItem("myLeads", JSON.stringify(myLeads) )
   renderLeads();
-  
+
   console.log( localStorage.getItem("myLeads") )
 });
 
 function renderLeads() {
   let listItems = "";
   for (let i = 0; i < myLeads.length; i++) {
-    // Create link with target="_blank" to open in a new tab
+    // link with target="_blank" to open in a new tab
     listItems += `
         <li>
             <a target='_blank' href='${myLeads[i]}'>${myLeads[i]}</a>
