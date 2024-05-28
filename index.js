3a11438ed86d5409e108cde237aac5364b2e4933
Yevhen Surfer <23skopets@gmail.com>
2024-05-28 11:03:42 +0200

    add delete-btn

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 let myLeads = [];
-const inputEl = document.getElementById("input-el");
-const inputBtn = document.getElementById("input-btn");
-const ulEl = document.getElementById("ul-el");
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const deleteBtn = document.getElementById("delete-btn")
+const ulEl = document.getElementById("ul-el")
 
-// localStorage.clear()
+
+// ["lead1", "lead2"] or null
 let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
 console.log(leadsFromLocalStorage)
+
+// Check if leadsFromLocalStorage is truthy
+// If so, set myLeads to its value and call renderLeads()
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
+
+// Listen for double clicks on the delete button
+// When clicked, clear localStorage, myLeads, and the DOM
+deleteBtn.addEventListener("dblclick", function() {
+    console.log("double clicked!")
+    localStorage.clear()
+    myLeads = []
+    renderLeads()
+})
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
